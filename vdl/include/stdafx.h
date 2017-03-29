369819b13716e37517932245b9e903bca8fa1532
Unknown <qscx9512@jbnu.ac.kr>
2017-03-30 01:25:07 +0900

    Changed XML parser to pugixml

--- vdl/include/stdafx.h
+++ vdl/include/stdafx.h
@@ -2,12 +2,9 @@
 #include "vdl.h"
 #include "../module/module.h"
 
-//Apache Xerces-c Headers
-#include <xercesc/parsers/XercesDOMParser.hpp>
-#include <xercesc/dom/DOM.hpp>
-#include <xercesc/sax/HandlerBase.hpp>
-#include <xercesc/util/XMLString.hpp>
-#include <xercesc/util/PlatformUtils.hpp>
+//PugiXML Header
+#include <pugixml/pugixml.hpp>
+#include <pugixml/pugiconfig.hpp>
 
 //ICU Header
 #include <unicode/ustream.h>
