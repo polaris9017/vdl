a851131c786f5e10bc9439b19636298e028abb86
Unknown <qscx9512@jbnu.ac.kr>
2017-03-30 01:39:52 +0900

    Main version up to 2.0.0

--- vdl/include/vdl.h
+++ vdl/include/vdl.h
@@ -3,7 +3,6 @@
 
 #include <iostream>
 #include <string>
-#include <sstream>
 #include <vector>
 #include <regex>
 #include <iomanip>
@@ -22,8 +21,9 @@
 #define TYPE_VLIVE_LIVE 0x12
 #define TYPE_TVCAST 0x13
 #define TYPE_TVPOT 0x14
-#define TYPE_FB 0x15
-#define TYPE_INSTAGRAM 0x16
+#define TYPE_KAKAO 0x15
+#define TYPE_FB 0x16
+#define TYPE_INSTAGRAM 0x17
 
 #define POST_FFMPEG 0x20
 
@@ -42,6 +42,7 @@ namespace VDLDefault {
 	const string URL_VLIVE_CH = "channels.vlive.tv";
 	const string URL_NAVER = "tv.naver.com";
 	const string URL_DAUM = "tvpot.daum.net";
+	const string URL_KAKAO = "tv.kakao.com";
 	const string URL_FACEBOOK = "facebook.com";
 	const string URL_TWITTER = "twitter.com";
 
@@ -53,10 +54,14 @@ namespace VDLDefault {
 	const string URL_INTERNAL_VLIVE_LIVE = "http://www.vlive.tv/video/init/view";
 	const string VLIVE_CH_INTERNAL_INFO_JSON = "http://api.vfan.vlive.tv/vproxy/channelplus/decodeChannelCode";
 	const string VLIVE_CH_INTERNAL_VOD_LIST = "http://api.vfan.vlive.tv/vproxy/channelplus/getChannelVideoList";
+	const string URL_INTERNAL_DAUM_RESOL = "http://videofarm.daum.net/controller/api/closed/v1_2/IntegratedMovieData.json";
+	const string URL_INTERNAL_DAUM_META = "http://tvpot.daum.net/clip/ClipInfoXml.do";
+	const string URL_INTERNAL_DAUM_CDN = "http://videofarm.daum.net/controller/api/open/v1_2/MovieLocation.apixml";
+	const string URL_INTERNAL_KAKAO_API = "http://tv.kakao.com/api/v1/ft/cliplinks/";
 
 	//Internal Constants
-	const string VDL_CODENAME = "R U Ready";
-	const string VDL_VERSION = "019903";
+	const string VDL_CODENAME = "WoW";
+	const string VDL_VERSION = "020000";
 	const string VLIVE_VIDEO_PARAM = "\\bvlive\\.video\\.init\\(([^)]+)";
 	const string VLIVE_VIDEO_CREATOR = "<div[^>]+class=\"info_area\"[^>]*>\\s*<a\\s+[^>]*>([^<]+)";
 	const string VLIVE_VIDEO_TITLE = "<meta[^\\n]+=\\\"og:title\\\" content=\\\"([^]+)\"\\/>[^]+\\\"og:url\"";
@@ -68,23 +73,25 @@ namespace VDLDefault {
 	const string VLIVE_CH_APP_JS = "<script[^>]+src=[\\\"\\'](http.+?\\/app\\.js)";
 	const string VLIVE_CH_APPID = "Global\\.VFAN_APP_ID\\s*=\\s*[\\'\"]([^\\'\"]+)[\\'\"]";
 	const string NAVER_MID = "var rmcPlayer = new nhn\\.rmcnmv\\.RMCVideoPlayer\\(\\\"(.+?)\\\", \\\"(.+?)\\\"";
+	const string DAUM_CLIPID = "tvpot\\.daum\\.net[^]+v\\/(.+)";
+	const string KAKAO_CLIPID = "tv\\.kakao\\.com[^]+cliplink\\/(.+)";
 
 	//Functions
-	void printInit();
-	bool Download(string url, wstring fname);
-	string print(string str, unsigned int level = 0);
-	string print(wstring str, unsigned int level = 0);
-	unsigned int Categorize(string url);
-	cpr::Response LoadPage(string url);
-	cpr::Response LoadPage(string url, cpr::Parameters param);
-	cpr::Response LoadPage(string url, cpr::Payload param);
-	string replace(string str, string r, string sbst);
+	void printInit();   //Show version
+	bool Download(string url, wstring fname);  //Download video(because of memory problem in cpr library)
+	string print(string str, unsigned int level = 0);  //Print string(std::string)
+	string print(wstring str, unsigned int level = 0);  //Print string(std::wstring)
+	unsigned int Categorize(string url);  //Categorize URL
+	cpr::Response LoadPage(string url);  //Load page(no args)
+	cpr::Response LoadPage(string url, cpr::Parameters param);  //Load page(args with GET method)
+	cpr::Response LoadPage(string url, cpr::Payload param);  //Load page(args with POST method)
+	string replace(string str, string r, string sbst);  
 	void decodeHtmlEntity(wstring &str);
 	size_t write_data(char *ptr, size_t size, size_t nmemb, void *userdata);
 	string wcs_to_mbs(std::wstring const& str, std::locale const& loc = std::locale(""));
 }
 
-
+//Main module
 class VDLModule {	
 protected:
 	int PostProcess(wstring f, size_t flag) {
@@ -96,16 +103,14 @@ public:
 	VDLModule(unsigned int major = VER_MAJOR, unsigned int minor = VER_MINOR, unsigned int rev = VER_REV) {
 		ver[0] = major, ver[1] = minor, ver[2] = rev;
 	}
-	void printInit();
-	void setUrl(string url);
-	bool LoadModule(uint16_t magic);
-	vector<string> Split(string s, const string &re);
-	virtual vector<string> FetchVideoList();
-	virtual vector<string> Parse(const string &s, const string r);
-	Json::Value ParseJson(const string & s);
-	friend ostream & operator<<(ostream & os, VDLModule & m);
-	virtual bool Run() { return false; }
+	void printInit();                                    //Print Initial string
+	void setUrl(string url);                        //Set given URL
+	bool LoadModule(uint16_t magic); //Load determined module
+	vector<string> Split(string s, const string &re);   //Split text using regular expression
+	virtual vector<string> FetchVideoList();  //Fetch video list from channel(not used in this class)
+	virtual vector<string> Parse(const string &s, const string r);   //Parse string using regular expression
+	Json::Value ParseJson(const string & s);   //Parse json string using JsonCpp library
+	friend ostream & operator<<(ostream & os, VDLModule & m);   //Operator for print version
+	virtual bool Run() { return false; }  //Run module
 };
-
-
 #endif // !VDL_H
